66fce451879667218190fbba30c561719ddc0204
Andrea Mattana <a.mattana80@gmail.com>
2022-04-13 17:06:34 +0200

    updated version number

--- skalab.py
+++ skalab.py
@@ -16,8 +16,8 @@ __copyright__ = "Copyright 2022, Istituto di RadioAstronomia, Radiotelescopi di 
 __author__ = "Andrea Mattana"
 __credits__ = ["Andrea Mattana"]
 __license__ = "GPL"
-__version__ = "1.0.7"
-__release__ = "2022-03-04"
+__version__ = "1.1.0"
+__release__ = "2022-04-13"
 __maintainer__ = "Andrea Mattana"
 
 import gc
