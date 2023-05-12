4f82ffebe6578c8a490b0f17e5f8cafc68a8dc79
amattana <a.mattana80@gmail.com>
2023-05-12 17:39:53 +0200

    Testing Station Init Log

--- skalab_log.py
+++ skalab_log.py
@@ -233,9 +233,12 @@ class SkalabLog(QtWidgets.QMainWindow):
 
     def writeLogWarning(self):
         if self.logInfo.msgQueue:
-            l, n, m = self.logWarning.msgQueue[0]
-            self.logWarning.msgQueue = self.logWarning.msgQueue[1:]
-            self.logWarning.updateBox(l, n, m)
+            try:
+                l, n, m = self.logWarning.msgQueue[0]
+                self.logWarning.msgQueue = self.logWarning.msgQueue[1:]
+                self.logWarning.updateBox(l, n, m)
+            except:
+                print("Raised Exception: ", self.logWarning.msgQueue)
 
     def writeLogError(self):
         if self.logInfo.msgQueue:
